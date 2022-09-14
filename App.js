26aa827ba1aeb3f5b678163d10fd65f10b4ca2b2
Novita Eka Widyastuti <novitaekawidyastuti@gmail.com>
2022-09-15 06:47:14 +0700

    template

--- App.js
+++ App.js
@@ -1,12 +1,12 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import { ThemeProvider } from './src/shared/context/ThemeContext';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Surpeedz Project</Text>
-      <StatusBar style="auto" />
-    </View>
+   <ThemeProvider>
+    
+   </ThemeProvider>
   );
 }
 
