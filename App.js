42e83bec2a57acf127d77e2e556b7fe8ce8caee4
Yurham Afif <yurhamafif.edu@gmail.com>
2022-09-15 16:44:46 +0700

    [add] reusable form text and password input

--- App.js
+++ App.js
@@ -4,14 +4,17 @@ import SignIn from './src/features/signin/SignIn';
 import SignUp from './src/features/signup/SignUp';
 import WelcomePage from './src/features/welcome/WelcomePage';
 import { ThemeProvider } from './src/shared/context/ThemeContext';
+import { themeRnUILib } from './src/shared/Theme-rnUILib';
+
+themeRnUILib();
 
 export default function App() {
   return (
    <ThemeProvider>
       <SignIn />
-      <SignUp />
+      {/* <SignUp />
       <WelcomePage />
-      <SellerPage />
+      <SellerPage /> */}
       
    </ThemeProvider>
   
